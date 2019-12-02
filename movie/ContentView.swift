33252fdcfdaf7d18e730b4c25cbebb6abba52c6b
ecem mine özyedierler <ecem@rotatech.com.tr>
2019-12-02 22:41:56 +0300

    Initial Commit

---
+++ movie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  movie
+//
+//  Created by ecem mine özyedierler on 2.12.2019.
+//  Copyright © 2019 ecem mine özyedierler. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
